33a0b5bde4a32a23c9b662d974e518ae1cd23638
Viviane Borges <vivianeborges353@gmail.com>
2026-04-29 21:49:44 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,105 @@
+# #atividade 01
+# print("teste")
+# contador = 1
+
+# while contador<=10:
+#     print(contador)
+#     if contador== 5:
+#         break
+#     contador +=1
+
+
+
+#     senha = ""
+#     while true:
+#         senha = input("digite a senha:")
+
+#         if senha == "1234":
+#             print("acesso liberado")
+#             break
+#         print("senha incorreta")
+
+# numero = 1
+# while numero <= 5:
+#     if numero== 2:
+#         continue
+#     print(numero)
+#     numero += 1
+
+    # contador=0
+    # while contador < 10:
+    #     contador+=1
+    #     if contador % 2 ==1:
+    #         continue
+    #     print(contador)
+
+    #     #atividade 01
+    #     numero = 1
+    #     while numero <= 10:
+    #         print(numero)
+
+    #         if numero == 6:
+    #                 break
+    #         numero += 1
+
+    #         #atividade02
+    #         numero =1
+    #         while numero <=10:
+
+    #             if numero ==5:
+    #                 numero+=1
+    #                 continue
+    #             print(numero)
+    #             numero +=1
+# #atividade 03
+# numero=1
+# while numero <=20:
+#     if numero ==15:
+#         break
+#     if numero % 2 == 0
+#         numero +=1
+#         continue
+#     print(numero)
+#     numero+=1 
+
+# #atividade 04
+# while True:
+#     produto = input("digite o nome do produto:")
+
+#     if produto == "fim":
+#         print("cadastro finalizado")
+#         break
+#     print("produto cadastrado",produto)
+
+# #atividade 05
+# soma =0
+# while True:
+#     numero = int(input("digite um numero:"))
+#     soma += numero
+
+#     if soma >= 20:
+#         break
+#     print("total",soma)
+
+#atividade 06
+# soma=0
+# while True:
+#     numero = int(input(digite um numero:))
+#     soma += numero
+
+#     if soma >= 50:
+#         print("limite atingido!")
+#         print("total:", soma)
+#         break
+
+# #atividade 07
+# while True:
+#     senha = input("digite a senha:")
+#     if senha == "teste":
+#         print("acesso liberado")
+#         break
+#     else:
+#         print("senha incorreta")
+ 
+
+
